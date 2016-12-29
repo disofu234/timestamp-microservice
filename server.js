a95772f9e5c451b0980df2543c5f3e2eba20b0cc
Diego <disofu234@gmail.com>
2016-12-29 19:53:37 +0000

    added api

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 var express = require("express");
+var path = require("path");
 
 var app = express();
 
+var months = new Array(	"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+
 app.get("/", function(req, res){
-    res.end("Hello World!"); 
+    res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.get("/:id", function(req, res){
+    var date_param = req.params.id;
+    
+    if (!isNaN(date_param)) {
+        date_param = +date_param * 1000;
+    } else {
+        date_param = date_param.split("%").join(" ");
+    }
+    
+    var date = new Date(date_param);
+    
+    var json = {
+        "unix": date.getTime() / 1000,
+        "natural": months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear()
+    };
+    
+    res.json(json);
 });
 
 app.listen(8080);
